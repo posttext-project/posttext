2db27bccdf68b71b6bf3a14a76c1c2aa8760685b
clitetailor <ducnhatphung@gmail.com>
2019-06-30 16:51:11 +0700

    chore: build config

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,28 +1,25 @@
-import { spawn } from 'child_process'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 import del from 'del'
 import gulp from 'gulp'
 import ts from 'gulp-typescript'
 import path from 'path'
-import { argv } from 'yargs'
 import sourcemaps from 'gulp-sourcemaps'
+
+const TEST_DIR = path.resolve(__dirname, '_test')
+const LIB_DIR = path.resolve(__dirname, 'lib')
 
 const tsProject = ts.createProject('tsconfig.json')
 
-function outDir() {
-  return path.resolve(argv.out || 'out')
+export async function clean(callback) {
+  await del(LIB_DIR + '/**')
+  callback()
 }
 
-function outFile() {
-  return path
-    .join(
-      outDir(),
-      path.relative('src', argv.target || 'src/index.ts')
-    )
-    .replace(/\.ts$/, '.js')
-}
-
-export function clean(callback) {
-  del(outDir() + '/**').then(() => callback())
+export async function cleanTest(callback) {
+  await del(TEST_DIR + '/**')
+  callback()
 }
 
 export const build = gulp.series(clean, function() {
@@ -31,13 +28,14 @@ export const build = gulp.series(clean, function() {
     .pipe(sourcemaps.init())
     .pipe(tsProject())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(outDir()))
+    .pipe(gulp.dest(LIB_DIR))
 })
 
-export const run = gulp.series(build, function(callback) {
-  spawn('node.exe', [outFile()], {
-    stdio: 'inherit'
-  })
-
-  callback()
+export const buildTest = gulp.series(cleanTest, () => {
+  return tsProject
+    .src()
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(TEST_DIR))
 })
