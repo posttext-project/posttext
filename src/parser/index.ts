4804dca4736a4738f04fbe77a026a0729a0a619e
clitetailor <ducnhatphung@gmail.com>
2019-12-17 23:39:45 +0700

    feat: parse verbatim

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -54,38 +54,8 @@ export class Parser {
 
     this.skip(cursor)
 
-    if (cursor.startsWith('{')) {
-      cursor.next(1)
-    } else {
-      cursor.moveTo(marker)
-
-      return null
-    }
-
-    const children: Node[] = []
-    const tempMarker = cursor.clone()
-    while (!cursor.startsWith('}') && !cursor.isEof()) {
-      const node =
-        this.parseTag(cursor) || this.parseText(cursor)
-
-      if (node === null) {
-        cursor.moveTo(tempMarker)
-
-        break
-      }
-
-      children.push(node)
-      tempMarker.moveTo(cursor)
-    }
-
-    if (cursor.startsWith('}')) {
-      cursor.next(1)
-    } else {
-      cursor.moveTo(marker)
-
-      return null
-    }
-
+    const children = this.parseVerbatimBlock(cursor) ?? this.parseBlock(cursor)
+    
     this.skip(cursor)
 
     if (cursor.startsWith(';')) {
@@ -118,6 +88,98 @@ export class Parser {
     }
 
     return null
+  }
+
+  parseBlock(cursor: Cursor): Node[] {
+    const marker = cursor.clone()
+
+    if (cursor.startsWith('{')) {
+      cursor.next(1)
+    } else {
+      cursor.moveTo(marker)
+
+      return null
+    }
+
+    const children: Node[] = []
+    const tempMarker = cursor.clone()
+    while (!cursor.startsWith('}') && !cursor.isEof()) {
+      const node =
+        this.parseTag(cursor) || this.parseText(cursor)
+
+      if (node === null) {
+        cursor.moveTo(tempMarker)
+
+        break
+      }
+
+      children.push(node)
+      tempMarker.moveTo(cursor)
+    }
+
+    if (cursor.startsWith('}')) {
+      cursor.next(1)
+    } else {
+      cursor.moveTo(marker)
+
+      return null
+    }
+
+    return children
+  }
+
+  parseVerbatimBlock(cursor: Cursor): Node[] | null {
+    const marker = cursor.clone()
+    const verbatimDecorator = cursor.exec(/=*/gy)[0]
+
+    if (verbatimDecorator.length === 0) {
+      return null
+    } else {
+      cursor.next(verbatimDecorator.length)
+    }
+
+    if (cursor.startsWith('{')) {
+      cursor.next(1)
+    } else {
+      cursor.moveTo(marker)
+
+      return null
+    }
+
+    const children: Node[] = []
+    const startMarker = cursor.clone()
+    const endMarker = cursor.clone()
+    do {
+      if (cursor.startsWith('}')) {
+        endMarker.moveTo(cursor)
+
+        cursor.next(1)
+
+        const execArr = cursor.exec(/=*/gy)
+        if (execArr[0].length === verbatimDecorator.length) {
+          cursor.next(execArr[0].length)
+
+          children.push({
+            type: 'Text',
+            value: startMarker.takeUntil(endMarker)
+          })
+
+          break
+        }
+
+        continue
+      }
+
+      cursor.next(1)
+
+      if (cursor.isEof()) {
+        cursor.moveTo(marker)
+
+        return null
+      }
+    } while (!cursor.isEof())
+
+    return children
   }
 
   parseText(cursor: Cursor): TextNode | null {
