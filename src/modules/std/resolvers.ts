77ba1dbbc7004ac8a4460a51c9fc659212ca504c
Clite Tailor <clitetailor@users.noreply.github.com>
2020-10-23 01:15:13 +0700

    Add license headers to source code files (#32)

--- src/modules/std/resolvers.ts
+++ src/modules/std/resolvers.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import Prism from 'prismjs'
 import loadLanguages from 'prismjs/components/'
 import stripIndent from 'strip-indent'
