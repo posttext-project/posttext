e0b052fdea85b4399cadef803a4701334212fc67
clitetailor <ducnhatphung@gmail.com>
2020-10-23 01:08:43 +0700

    Add license headers to source code files

--- src/cli/serve.ts
+++ src/cli/serve.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 import ws from 'ws'
 import fs from 'fs-extra'
 import Koa from 'koa'
--- src/printer/helpers/handlebars.ts
+++ src/printer/helpers/handlebars.ts
@@ -1,4 +1,8 @@
-import { compile as _compile } from 'handlebars'
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+ import { compile as _compile } from 'handlebars'
 import { default as memoizee } from 'memoizee'
 
 export const compile: <T = any>(
