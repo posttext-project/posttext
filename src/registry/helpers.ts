5d94ac16ec0ab228918c682cd8674766fa4cf604
clitetailor <ducnhatphung@gmail.com>
2021-03-30 01:14:21 +0700

    Add license header

--- src/registry/helpers.ts
+++ src/registry/helpers.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 export function createAsyncIter<T, TReturn = any, TNext = any>(
   callback: (iter: ResolverAsyncIter<T, TReturn, TNext>) => any
 ): {
